3d7ed98d3c7f3e6025ce11336a0e162e52f65ca1
Deepak Kumar Ram <deepak.kumar2305@gmail.com>
2023-02-08 19:09:05 +0530

    First insert in non-recommended basic way

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,13 +1,22 @@
 package com.spring.jdbc;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+    	JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+    	
+//    	INSERT
+    	String query = "INSERT INTO student(id,name,city) VALUES(?,?,?)";
+    	int result = jdbcTemplate.update(query, 456, "Deepak Ram", "Nagpur");
+    	System.out.println("Number of record inserted: "+result);
+    	
+    	((ConfigurableApplicationContext)context).close();
     }
 }
